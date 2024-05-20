e853911dd297b666ec933588f465d7a306323492
Jakub Majkowski <60348670+JakubMajkowski@users.noreply.github.com>
2024-05-20 13:49:35 +0200

    php fix 2

--- contactform.php
+++ contactform.php
@@ -1,15 +1,16 @@
 <?php
 
-if (isset($_POST['submit'])) {
     $firstname = $_POST['firstname'];
     $email = $_POST['email'];
     $message = $_POST['subject'];
 
-    $mailTo = "majkowski.jakub.praca@gmail.com";
-    $headers = "Od: " $mailFrom;
-    $txt = "Masz nową wiadomość od " .$name ",z e-korepetycje\n\n".$message;
+    $mailheader = "From:".$name."<".$email.">\r\n";
 
-    mail($mailTo, $subject, $txt, $headers);
-    header("Location: index.html");
-}
-?>
+    $recipient = "majkowski.jakub.praca@gmail.com";
+
+    mail($recipient, $subject, $message, $mailheader)
+    or die("Error!");
+
+    echo"message sent";
+
+    ?>
