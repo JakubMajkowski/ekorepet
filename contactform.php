2bfc491f4f179e2720240bf5604f266c5f5d5cc2
Jakub Majkowski <60348670+JakubMajkowski@users.noreply.github.com>
2024-05-20 13:44:42 +0200

    php fix

--- contactform.php
+++ contactform.php
@@ -10,5 +10,6 @@ if (isset($_POST['submit'])) {
     $txt = "Masz nową wiadomość od " .$name ",z e-korepetycje\n\n".$message;
 
     mail($mailTo, $subject, $txt, $headers);
-    header("Location: index.php?mailsend");
+    header("Location: index.html");
 }
+?>
