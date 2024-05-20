c42384ce7470fa3095e20e7d336243fff1dc1459
Jakub Majkowski <60348670+JakubMajkowski@users.noreply.github.com>
2024-05-20 13:37:02 +0200

    ver 1 php test

---
+++ contactform.php
@@ -0,0 +1,14 @@
+<?php
+
+if (isset($_POST['submit'])) {
+    $firstname = $_POST['firstname'];
+    $email = $_POST['email'];
+    $message = $_POST['subject'];
+
+    $mailTo = "majkowski.jakub.praca@gmail.com";
+    $headers = "Od: " $mailFrom;
+    $txt = "Masz nową wiadomość od " .$name ",z e-korepetycje\n\n".$message;
+
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: index.php?mailsend");
+}
